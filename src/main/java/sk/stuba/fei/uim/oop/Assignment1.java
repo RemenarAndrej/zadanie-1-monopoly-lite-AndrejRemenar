256d8c22b067234184747eddf7c470aeb553d237
Andrej Remenar <81436413+RemenarAndrej@users.noreply.github.com>
2021-03-26 18:51:40 +0100

    Update Assignment1.java

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -1,5 +1,10 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.Kocka;
+import sk.stuba.fei.uim.oop.Hra;
+
+import static sk.stuba.fei.uim.oop.Sanca.stupilNaSancu;
+
 public class Assignment1 {
     public static void main(String[] args) {
         Hra h = new Hra();
